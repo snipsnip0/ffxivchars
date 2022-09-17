431cf1f8f802dd9b1c817e192fb53b2ee3db7cb2
Bruno Dias <brunodias.snips@gmail.com>
2022-09-17 20:39:45 +0100

    aaaaaaaaaaaaaaaaaaa

---
+++ db/classes.js
@@ -0,0 +1,6 @@
+var classes = ["Paladin", "Warrior", "Dark Knight", "Gunbreaker",
+    "White Mage", "Scholar", "Astrologian", "Sage",
+    "Monk", "Dragoon", "Ninja", "Samurai", "Reaper",
+    "Bard", "Machinist", "Dancer",
+    "Black Mage", "Summoner", "Red Mage", "Blue Mage",
+];
---
+++ scripts/addCharacter.js
@@ -0,0 +1,31 @@
+
+var cardsElement = document.getElementsByClassName("cards")[0];
+
+var max = 0;
+
+function addChar() {
+    if (max == 10) return;
+
+
+    let name = document.createElement("p");
+    let classe = document.createElement("p");
+    let icon = document.createElement("i");
+
+    name.textContent = `${names[Math.floor(Math.random() * names.length)]} ${surnames[Math.floor(Math.random() * surnames.length)]}`;
+    classe.textContent = `${classes[Math.floor(Math.random() * classes.length)]}`;
+
+    name.classList.add("name");
+    classe.classList.add("class");
+    icon.classList.add("fa-solid");
+    icon.classList.add("fa-circle-user");
+
+    let character = document.createElement("div");
+    character.classList.add("card");
+    character.append(icon);
+    character.append(name);
+    character.append(classe);
+
+    cardsElement.append(character);
+
+    max++;
+}
